51b49e4d7a073a6f87e05b84ce5f5de4c60c0d0d
franciscoalvarez-cloud <fra.alvarezs@duocuc.cl>
2024-07-12 11:13:46 -0400

    archivo_examen_tranbersal

---
+++ exsamen.py
@@ -0,0 +1,19 @@
+import random
+
+trabajadores = [
+    "Juan Pérez”,”María García”,”Carlos López”,”Ana Martínez”,”Pedro Rodríguez”,”Laura Hernández”,”Miguel Sánchez”,”Isabel Gómez”,”Francisco Díaz”,”Elena Fernández"
+    ]
+def aplicacion_de_sueldo():
+    for enpleados in trabajadores:
+        enpleados["sueldo"] = random.randint(300.000 , 2500000)
+
+
+def clasificasion_sueldos():
+
+                                                                                                                  
+                                                                                                                  
+                                                                                                                  
+                                                                                                                  
+                                                                                                                  
+                                                                                                                  
+                                                                                                                                                                                                 
